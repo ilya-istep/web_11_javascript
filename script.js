dce299d6debc9bcf4f327ed0ba1de5c4d9e6a40f
Oleg Kudelkin <tryhard.work@mail.ru>
2022-02-20 23:59:10 +0300

    try to learn

--- script.js
+++ script.js
@@ -1,5 +1,11 @@
 'use strict';
+
 /*
+HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK
+HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK
+HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK
+HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK
+
 let personeName = 'Human';
 let botName     = 'Bot';
 
@@ -22,27 +28,62 @@ for(let index = 0; index >= 0; index++) {
     break;
 
   }
+
+HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK
+HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK
+HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK
+HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK
 } */
 
 
+let colors = [];
+let colorsIndex = 0;
 
+while(true){
+  let color = prompt();
+  let i = 0;
+  let flag = false;
 
-// let colors      = [],
-//     colorsIndex = 0;
+  while(i < colors.length){
+    if(colors[i] == color){
+      flag = true;
+      break;
+    }
+    i = i + 1;
+  }
 
-// let colorWrite;
+  if(flag == false){
+    colors[colorsIndex] = color;
+  }
 
-// while (true) {
-//   colorWrite = prompt('Введите цвет', 'красный');
+  if(color == 0 || color == null){
+    break;
+  }
 
-//   if (colorWrite && colorWrite != '' && colorWrite != null) {
-//     colors[colorsIndex] = colorWrite;
-//     colorsIndex++;
-//     console.log(colorsIndex);
+  colorsIndex = colorsIndex + 1;
+  console.log(colors);
+}
 
-//   } else {
-//     break;
-//   }
+/*
+1) Создаем переменные:
+  Пустой массив цветов.
+  Индекс массива цветов (colorsIndex).
 
-//   console.log(colors);
-// }
+2) Создаем бесконечный цикл.
+
+3) Создаем переменные в массиве:
+  Переменная, в которую помещаем цвет, который ввел пользователь.
+  Переменная итератор массива (i).
+  Переменная flag, которая отвечает за совпадения и присваивает себе значение false.
+
+4) Создаем условие, при котором цикл будет завершаться:
+  Если пользователь ввел пустую строку, ноль или null, то мы завершаем цикл.
+
+5) Создаем новый цикл в нашем цикле с условием:
+  Пока переменная итератор массива (i) меньше, чем длина массива, мы увеличиваем эту переменную на один.
+  5.1) В этом цикле добавляем условие: 
+    Если значение нашего массива colors[i] равна значению, введеному пользователем, то в переменную совпадения (flag) помещаем значение true и останавливаем этот цикл.
+
+6) В глобальный цикл добавляем еще одно условие:
+    Если проверка нашего совпадения (flag) равна false, то мы записываем значение, что ввел пользователь в массив с цветами в индекс массива цветов (colorsIndex)
+*/
