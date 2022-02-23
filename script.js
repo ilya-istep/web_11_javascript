4f149bc9fab205333dad8e9181899abd06dec44a
Oleg Kudelkin <tryhard.work@mail.ru>
2022-02-23 23:29:28 +0300

    Дополнил крайнее ДЗ

--- script.js
+++ script.js
@@ -40,61 +40,53 @@ for(let index = 0; index >= 0; index++) {
 // HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK 2
 // HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK 2
 
-let triangleDegreeQuestion = () => {
-  let question = prompt('Какой угол у катетов?');
-  if (question == 'девяносто' || 
-  question == 'девяносто градусов' || 
-  question == '90' || 
-  question == '90 градусов' || 
-  question == '90℃' || 
-  question == '90 ℃' || 
-  question == '90°' || 
-  question == '90 °') {
-    return 90;
-  }
-}
-
-if (triangleDegreeQuestion() == 90) {
-  let triangleSides = [];
-
-  let calcTrianglePerimeter = (a, b, c) => {
-    let halfPerimetr = (a + b + c) / 2;
-    let perimetr     = Math.sqrt(halfPerimetr * (halfPerimetr - a) * (halfPerimetr - b) * (halfPerimetr - c)).toFixed(3);
-
-    if (isNaN(perimetr)) {
-      return 'Такого треугольника не существует!';
-    } else {
-      return perimetr;
-    }
-  };
-
-
-  for (let i = 0; i < 3; i++) {
-    let sides = ['AB', 'BC', 'AC'];
-
-    triangleSides[i] = prompt(`Чему равна сторона ${sides[i]}?`);
-    
-    console.log(triangleSides);
-
-    if (triangleSides[i] == '' || triangleSides[i] == null) {
-      break;
+(function (){
+  let triangleDegreeQuestion = () => {
+    let question = prompt('Какой угол у катетов?');
+    if (question == 'девяносто' || 
+    question == 'девяносто градусов' || 
+    question == '90' || 
+    question == '90 градусов' || 
+    question == '90℃' || 
+    question == '90 ℃' || 
+    question == '90°' || 
+    question == '90 °') {
+      return 90;
     }
   }
-
-  console.log(calcTrianglePerimeter(+triangleSides[0], +triangleSides[1], +triangleSides[2]));
-} else {
-  console.log('Я забыл ТЗ...');
-}
-
-
-
-
-// console.log(triangleSides);
-
-
-
-
-
+  
+  if (triangleDegreeQuestion() == 90) {
+    let triangleSides = [];
+  
+    let calcTrianglePerimeter = (a, b, c) => {
+      let halfPerimetr = (a + b + c) / 2;
+      let perimetr     = Math.sqrt(halfPerimetr * (halfPerimetr - a) * (halfPerimetr - b) * (halfPerimetr - c)).toFixed(1);
+  
+      if (isNaN(perimetr)) {
+        return 'Такого треугольника не существует!';
+      } else {
+        return `Sп.т. = ${perimetr}кв.см`;
+      }
+    };
+  
+  
+    for (let i = 0; i < 3; i++) {
+      let sides = ['AB', 'BC', 'AC'];
+  
+      triangleSides[i] = prompt(`Чему равна сторона ${sides[i]}?`);
+      
+      console.log(triangleSides);
+  
+      if (triangleSides[i] == '' || triangleSides[i] == null) {
+        break;
+      }
+    }
+  
+    console.log(calcTrianglePerimeter(+triangleSides[0], +triangleSides[1], +triangleSides[2]));
+  } else {
+    console.log('Это не прямоугольный треугольник');
+  }
+})();
 
 // HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK 2
 // HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK 2
