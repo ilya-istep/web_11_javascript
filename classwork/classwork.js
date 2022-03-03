2ff3a779abe078f51c5961c4b937f58d4c914dd6
Oleg Kudelkin <tryhard.work@mail.ru>
2022-03-03 21:17:54 +0300

    1

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,1 +1,18 @@
-'use strict';
+let btn = document.querySelector('.btn');
+
+let animation = (event) => {
+
+  if (btn.classList.contains('btn-animation')) {
+    btn.classList.toggle('btn-animation-state');
+  }
+
+  btn.classList.add('btn-animation');
+}
+
+btn.addEventListener('click', animation);
+
+btn.addEventListener('dblclick', () => {
+  btn.classList.remove('btn-animation');
+});
+
+
