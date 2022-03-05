d5f3df9832a8efe55f971974d1a4cd203a8afea3
Oleg Kudelkin <tryhard.work@mail.ru>
2022-03-05 17:42:06 +0300

    complete homework4

--- homework2/script.js
+++ homework2/script.js
@@ -7,62 +7,62 @@ console.log('HOMEWORK2 HOMEWORK2 HOMEWORK2 HOMEWORK2');
 // HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK 2
 // HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK 2
 
-(function (){
-  let triangleDegreeAnswer = triangleDegreeQuestion();
 
-  function triangleDegreeQuestion () {
-    let question = prompt('Какой угол у катетов?');
 
-    if (question != '' && question != null) {
-      question = question.toLowerCase();
-    }
+let triangleDegree = [];
 
-    if (question == 'девяносто' || 
-    question == 'девяносто градусов' || 
-    question == '90' || 
-    question == '90 градусов' || 
-    question == '90℃' || 
-    question == '90 ℃' || 
-    question == '90°' || 
-    question == '90 °') {
-      return 90;
-    } else {
-      return '';
-    }
+for (let i = 0; i < 3; i++) {
+  triangleDegree[i] = +prompt(`Чему равны стороны треугольника?`);
+}
+
+let calcAngle = (a, b, c) => {
+  let cosA = radianToDegree(((a ** 2) + (c ** 2) - (b ** 2)) / (2 * a * c));
+  let cosB = radianToDegree(((a ** 2) + (b ** 2) - (c ** 2)) / (2 * a * b));
+  let cosC = radianToDegree(((b ** 2) + (c ** 2) - (a ** 2)) / (2 * c * b));
+
+  // console.log(`Угол A равен ${cosA}`);
+  // console.log(`Угол B равен ${cosB}`);
+  // console.log(`Угол С равен ${cosC}`);
+
+  if (cosA == cosB 
+  || cosA == cosC 
+  || cosB == cosC) {
+    return 90;
   }
 
-  if (triangleDegreeAnswer == 90) {
-    let triangleSides = [];
-  
-    let calcTrianglePerimeter = (a, b, c) => {
-      let halfPerimetr = (a + b + c) / 2;
-      let perimetr     = Math.sqrt(halfPerimetr * (halfPerimetr - a) * (halfPerimetr - b) * (halfPerimetr - c)).toFixed(1);
-  
-      if (isNaN(perimetr)) {
-        return 'Такого треугольника не существует!';
-      } else {
-        return `Sп.т. = ${perimetr}кв.см`;
-      }
-    };
-  
-  
-    for (let i = 0; i < 3; i++) {
-      let sides = ['AB', 'BC', 'AC'];
-  
-      triangleSides[i] = prompt(`Чему равна сторона ${sides[i]}?`);
-      
-      console.log(triangleSides);
-  
-      if (triangleSides[i] == '' || triangleSides[i] == null) {
-        break;
-      }
-    }
-  
-    console.log(calcTrianglePerimeter(+triangleSides[0], +triangleSides[1], +triangleSides[2]));
+  function radianToDegree(radian) {
+    return radian = (radian * 180) / Math.PI;
+  }
+}
+
+let calcTrianglePerimeter = (a, b, c) => {
+  let halfPerimetr = (a + b + c) / 2;
+  let perimetr     = Math.sqrt(halfPerimetr * (halfPerimetr - a) * (halfPerimetr - b) * (halfPerimetr - c)).toFixed(1);
+
+  if (isNaN(perimetr) || perimetr == 0) {
+    return 'Такого треугольника не существует!';
   } else {
-    console.log('Это не прямоугольный треугольник');
+    return `Sп.т. = ${perimetr}кв.см`;
   }
-})();
+};
+
+if (calcAngle(+triangleDegree[0], +triangleDegree[1], +triangleDegree[2]) == 90) {
+  let triangleSides = [];
+
+  for (let i = 0; i < 3; i++) {
+    let sides = ['AB', 'BC', 'AC'];
+
+    triangleSides[i] = prompt(`Чему равна сторона прямоугольного треугольника ${sides[i]}?`);
+
+    if (triangleSides[i] == '' || triangleSides[i] == null) {
+      break;
+    }
+  }
+
+  console.log(calcTrianglePerimeter(+triangleSides[0], +triangleSides[1], +triangleSides[2]));
+} else {
+  console.log('Это не прямоугольный треугольник');
+}
 
 // HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK 2
 // HOMEWORK HOMEWORK HOMEWORK HOMEWORK HOMEWORK 2
--- script.js
+++ script.js
@@ -1,2 +1,17 @@
 'use strict';
 
+const NUM = [-1, 2, -3, 4, -5, 6, -7, 0, -8];
+
+const signCount = function(array) {
+  let res = 0;
+
+  for (let index = 0; index < array.length; index++) {
+    if (Math.sign(array[index] !== 1) && array[index] !== 0) {
+      res++;
+      console.log(Math.sign(array[index]));
+    }
+  }
+  return res;
+}
+
+console.log(signCount(NUM));
