88a094d524918ef17944ca5083253a37e1c78714
Oleg Kudelkin <tryhard.work@mail.ru>
2022-03-22 18:07:43 +0300

    test

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,8 +1,3 @@
 'use strict';
 
-let navMenuBtn = document.querySelector('.nav-menu-btn'),
-    mainWrapp = document.querySelector('.main-wrapper');
 
-navMenuBtn.addEventListener('click', () => {
-  mainWrapp.classList.toggle('active');
-});
