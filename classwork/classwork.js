89685ccf41b2d929970a147096252186633999a3
Oleg Kudelkin <tryhard.work@mail.ru>
2022-03-24 21:13:44 +0300

    a

--- classwork/classwork.js
+++ classwork/classwork.js
@@ -1,98 +1,73 @@
 'use strict';
 
+let listOffers = document.querySelector('.list-offers');
+let offersFilter = document.querySelector('.offers-filter');
 
-/*
-for(let btn of tabsBtns) {
-  btn.addEventListener('click', function(event) {
-    let elementParentActive = document.querySelector('.tabs-nav li.active');
-    elementParentActive.classList.remove('active');
+let offers = [
+  {position: 'fornt-end', salary: 100000},
+  {position: 'back-end', salary: 200000},
+  {position: 'back-end', salary: 250000},
+  {position: 'back-end', salary: 290000},
+  {position: 'fullstack', salary: 300000},
+];
 
-    let parentOfbtn = event.target.parentElement;
-    parentOfbtn.classList.add('active');
-
-    let elementBlockActive = document.querySelector('.tabs-content .active');
-    elementBlockActive.classList.remove('active');
-
-    let hashBtn = '#' + parentOfbtn.dataset.block;
-
-    let Elementblock = document.querySelector(hashBtn);
-    Elementblock.classList.add('active');
-  });
-}
-*/
+if (offers.length > 0) {
+  setTimeout(renderListOffers, 2000);
 
 
-let tabs = {
-  tabsParent: '#tabs1',
-  objectTab: null,
-  switched: function() {
-    let context = this;
-
-    this.objectTab.addEventListener('click', (event) => {
-      if (event.target.closest('.btn')) {
-        let parentElement = event.target.parentElement;
-
-        if (parentElement.tagName != 'LI') {
-          context.exeption(null);
-          return 0;
-        }
-
-        let parentElementActive = context.objectTab.querySelector('.tabs-nav li.active');
-
-        context.removeClass(parentElementActive);
-        context.addClass(parentElement);
-
-        let valueAttribute = parentElement.dataset.block;
-
-        if (valueAttribute === null) {
-          context.exeption(null);
-          return 0;
-        }
-        
-        let blockElementActive = context.objectTab.querySelector('#' + valueAttribute);
-
-        if (blockElementActive === null) {
-          context.exeption(null);
-          return 0;
-        } else {
-          let blockActive = context.objectTab.querySelector('.tabs-content div.active');
-
-          context.removeClass(blockActive);
-          context.addClass(blockElementActive);
-        }
-      }
-    });
-  },
-  addClass: (element) => {
-    element.classList.add('active');
-  },
-  removeClass: (element) => {
-    element.classList.remove('active');
-  },
-  exeption: (object) => {
-    if(object === null) {
-      console.log('error');
-      return false;
-    } else {
-      return true;
-    }
-  },
-  initialization: function() {
-    console.log('Запускаем табы');
-
-    this.objectTab = document.querySelector(this.tabsParent);
-
-    if(this.exeption(this.objectTab) == false) {
-      return 0;
-    }
-
-    this.switched();
-  },
+} else {
+  listOffers.innerHTML = 'Нет вакансий';
 }
 
-tabs.initialization();
 
-let tabs2 = tabs;
-tabs2.tabsParent = '#tabs2';
 
-tabs2.initialization();
+function renderFilters() {
+  filtersPositions();
+  filtersSalary();
+}
+
+function filtersPositions() {
+
+  let filterPositions = document.querySelector('.filter-positions');
+
+  let positions = new Set();
+
+  for(let offer of offers) {
+    positions.add(offer.position);
+  }
+
+  for (let position of positions) {
+    filterPositions.innerHTML = filterPositions.innerHTML + `
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" value="" id="${position}">
+      <label class="form-check-label" for="${position}">
+        ${position}
+      </label>
+    </div>`
+  }
+}
+
+function filtersSalary() {
+  
+}
+
+function renderListOffers() {
+  let listOffersStr = '';
+  for(let offer of offers) {
+    listOffersStr = listOffersStr +`<li><strong>${offer.position}</strong> ${offer.salary}</li>`;
+  }
+  listOffersStr = '<ul>' + listOffersStr + '</ul>';
+  listOffers.innerHTML = listOffersStr;
+  renderFilters();
+}
+
+// let qustion = prompt('Желаемая должность', 'back-end'),
+//     qustion2 = +prompt('Желаемая зарплата', '230000');
+
+// offers.forEach(item => {
+//   if (qustion == 'back-end' && qustion2 > 225000 && qustion2 < 270000) {
+//     console.log(item.position, item.salary);
+//   } else {
+//     console.log('error');
+//   }
+// });
