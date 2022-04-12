0dbd48f8de302553373e7b429961d8d33785621e
Oleg Kudelkin <tryhard.work@mail.ru>
2022-04-12 19:33:08 +0300

    create typing

--- chat/js/classwork.js
+++ chat/js/classwork.js
@@ -3,9 +3,13 @@
 
 	let messages = [];
 
+	let statusWriteElement = document.querySelector('.status-write');
 	let sendBtn = document.querySelector('.send-btn');
 	let textInput = document.querySelector('.input-message');
 	let chatWindow = document.querySelector('.chat-history > ul');
+
+	let intervalID = null;
+	let flag = false;
 
 	function addZero(num) {
 		if(num >= 10) {
@@ -18,6 +22,24 @@
 	function scrollingBottom(element) {
 		element.scrollTop = element.scrollTop + 50000;
 		element.scrollTo(0, element.scrollTop);
+	}
+
+	function dynamicWrite() {
+		let step = 1;
+	
+		let intervalID = setInterval(() => {
+			if (step < 4) {
+				statusWriteElement.innerHTML += '.';
+				step++;
+
+			} else {
+				statusWriteElement.innerHTML = 'Печатает';
+				step = 1;
+
+			}
+		}, 300);
+
+		return intervalID;
 	}
 
 	let sendMessage = (event) => {
@@ -42,11 +64,41 @@
 			</li>
 			`;
 			textInput.value = '';
-			
 			scrollingBottom(chatWindow);
+
+			statusWriteElement.innerHTML = '';
+			clearInterval(intervalID);
 		}
 	}
-	
+
+
+	textInput.addEventListener('input', (event) => {
+
+		if (!flag) {
+			statusWriteElement.innerHTML = 'Печатает';
+			intervalID = dynamicWrite();
+
+			setTimeout(() => {
+				statusWriteElement.innerHTML = '';
+				flag = false;
+
+				clearInterval(intervalID);
+			}, 1500);
+			
+			flag = true;
+		}
+
+	});
+
+	textInput.addEventListener('blur', (event) => {
+
+		if (flag) {
+
+			statusWriteElement.innerHTML = '';
+			flag = false;
+		}
+	});
+
 	
 	sendBtn.addEventListener('click', sendMessage);
 	document.addEventListener('keypress', sendMessage);
