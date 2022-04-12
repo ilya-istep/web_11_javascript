51147fbedd42468063defbc9e6d8ff3a0469f042
Oleg Kudelkin <tryhard.work@mail.ru>
2022-04-12 21:10:53 +0300

    sdf

--- js/scripts.js
+++ js/scripts.js
@@ -1,218 +1,79 @@
 (function(){
-
 	'use strict';
 
-	// есть список оферов с "должностями" и "окладом" от кадрового агенства
-	// вывести в консоль только те оферы, которые подходят именно вам
+	class Users {
+		name = '';
+		avatar = '';
+		timeVizited = '';
 
-	const COUNT_OFFERS_TO_PAGE = 2;
-	let activePage = 1;
+		sendMessage() {
 
-	let offers = [
-		{
-			position: "Требуется front-end разработчик",
-			money: 100000,
-			city: "Нижний Новгород",
-			logo_company: "logo1.webp",
-			skills: "",
-			content: ""
-		},
-		{
-			position: "Требуется back-end разработчик",
-			money: 100000,
-			city: "Нижний Новгород",
-			logo_company: "logo2.webp",
-			skills: "",
-			content: ""
-		},
-		{
-			position: "Верстальщик / front-end разработчик",
-			money: 100000,
-			city: "Нижний Новгород",
-			logo_company: "logo2.webp",
-			skills: "",
-			content: ""
-		},
-		{
-			position: "Верстальщик",
-			money: 100000,
-			city: "Нижний Новгород",
-			logo_company: "logo1.webp",
-			skills: "",
-			content: ""
-		},
-		{
-			position: "Требуется back-end разработчик",
-			money: 100000,
-			city: "Нижний Новгород",
-			logo_company: "logo2.webp",
-			skills: "",
-			content: ""
-		},
-		{
-			position: "Требуется back-end разработчик",
-			money: 100000,
-			city: "Нижний Новгород",
-			logo_company: "logo2.webp",
-			skills: "",
-			content: ""
-		}
-		,
-		{
-			position: "Требуется back-end разработчик",
-			money: 100000,
-			city: "Нижний Новгород",
-			logo_company: "logo2.webp",
-			skills: "",
-			content: ""
-		}
-		,
-		{
-			position: "Требуется back-end разработчик",
-			money: 100000,
-			city: "Нижний Новгород",
-			logo_company: "logo2.webp",
-			skills: "",
-			content: ""
-		}
-	];
-
-	let listOffers = document.querySelector(".list_offers");
-	let offersFilters = document.querySelector(".offers_filter");
-
-	if(offers.length > 0){
-
-		setTimeout(()=>{
-			renderFilters();
-			renderListOffers();
-			listOffers.parentElement.innerHTML += renderPaginations();	
-			switchPagination();
-		}, 100);
-	}	
-	else{
-		listOffers.innerHTML = "Нет вакансий";
-	}
-
-	function renderFilters(){
-		filtersPositions();
-		filtersMoneys();
-	}
-
-	function filtersPositions(){
-
-		let filtersPositions = offersFilters.querySelector(".filters_positions");
-
-		let positions = new Set();
-
-		for(let offer of offers){
-			positions.add(offer.position);
-		}
-
-
-
-		for (let position of positions) {
-
-			filtersPositions.innerHTML = filtersPositions.innerHTML + `
-				<div class="form-check">
-					<input class="form-check-input position_field" type="checkbox" value="" id="${position}">
-					<label class="form-check-label" for="${position}">
-						${position}
-					</label>
-				</div>
-			`;
-		}
-
-		let positionFields = document.querySelectorAll(".position_field");
-
-		for(let positionField of positionFields){
-
-			positionField.addEventListener('input', (evt)=>{
-				console.log(1);
-			})
-		}
-
-	}
-
-	function filtersMoneys(){
-		
-	}
-
-	function renderPaginations(){
-
-		let countPage = Math.ceil(offers.length / COUNT_OFFERS_TO_PAGE);
-
-		let strNumbers = "";
-		for(let i = 1; i <= countPage; i++){
-			strNumbers += `<li class="page-item"><a class="page-link" href="#">${i}</a></li>`;
-		}
-
-
-		return `<nav aria-label="Page navigation example" class="pagination">
-					<ul class="pagination">
-						<li class="page-item">
-							<a class="page-link" href="#" aria-label="Previous">
-								<span aria-hidden="true">&laquo;</span>
-							</a>
-						</li>
-
-						${strNumbers}
-
-						<li class="page-item">
-							<a class="page-link" href="#" aria-label="Next">
-								<span aria-hidden="true">&raquo;</span>
-							</a>
-						</li>
-					</ul>
-				</nav>`;
-	}
-
-	function switchPagination(){
-		let paginationLinks = document.querySelectorAll(".pagination .page-link");
-		let paginationStart = document.querySelector('.pagination .page-item:first-child');
-		let paginationEnd = document.querySelector('.pagination .page-item:last-child');
-
-		for(let link of paginationLinks){
-			link.addEventListener('click', (evt)=>{
-				evt.preventDefault();
-				activePage = Number(evt.target.innerHTML);
-				renderListOffers();
-			})
 		}
 	}
 
-	function renderListOffers(){
-		let listOffersStr = "";
+	class Messages {
+		content = '';
+		timeCreate = '';
+		author = '';
 
-		let start_index;
-		if(activePage == 1){
-			start_index = 0;
+		renderMessages() {
+			this.author = new Users();
 		}
-		else{
-			start_index = (activePage - 1) * COUNT_OFFERS_TO_PAGE;
-		}
-
-
-		offers.forEach((offer, index)=>{
-
-			// COUNT_OFFERS_TO_PAGE - количество записей на странице
-			// activePage - выбранный номер страницы
-			// offers.length - всего записей
-
-			// поправить это условие
-			if( (index >= start_index) && (index < (start_index + COUNT_OFFERS_TO_PAGE)) ){
-				
-				listOffersStr += `<div class="item_offer">
-														<img src="images/${offer.logo_company}" alt="">
-														<div class="offer_info">
-															<h3>${offer.position}</h3>
-															<p class="city">${offer.city}</p>
-															<strong class="money">${offer.money} руб</strong>
-														</div>
-													</div>`;
-			}
-		});
-
-		document.querySelector(".list_offers").innerHTML = listOffersStr;
 	}
+
+	class Dialog {
+		messages = [];
+		user1 = '';
+		user2 = '';
+
+		renderDialog() {
+			this.user1 = new Users();
+			this.user1 = new Users();
+		}
+	}
+
+	class Chat {
+		users = null;
+
+		renderChat() {
+			let dialog = new Dialog();
+
+			dialog.renderDialog();
+		}
+	}
+
+	let chat = new Chat();
+	chat.renderChat();
+
+
+
+
+
+
+
+
+
+
+
+	// class Animal {
+	// 	paws = 4;
+
+	// }
+
+	// class Cat extends Animal {
+
+	// }
+
+	// class Dog extends Animal {
+
+	// }
+
+
+	// let cat1 = new Cat();
+
+	// console.log(cat1.paws);
+
+
+
 })();	
 
