b423de039f3eb0ace92e3098cb467da332c3d765
Ilya <ipoletuev@mail.ru>
2022-04-19 19:45:54 +0300

    Модули в js

---
+++ js/module_1.js
@@ -0,0 +1,16 @@
+let a = 10;
+
+
+let b = 20;
+
+export let mass = [a, b];
+
+function met(){
+    console.log("я функция");
+}
+
+class User{
+
+    name = "Andrey";
+    
+}
---
+++ js/tabs/tabs_content_module.js
@@ -0,0 +1,8 @@
+export default class TabsContent{
+    name = null;
+    content = null;
+
+    render(){
+
+    }
+}
