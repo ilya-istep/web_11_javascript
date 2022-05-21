4aa0a3b19b4de5278375c04bfc8ea981a07f78bd
kvisatz <kvisatz@mail.ru>
2022-05-21 13:17:58 +0300

    прелоадер для сайта keira

--- js/Cat.js
+++
@@ -1,7 +0,0 @@
-export default class Cat{
-	color = "black";
-}
-
-
-
-let a = 0;
--- js/tabs/tabs_content_module.js
+++
@@ -1,8 +0,0 @@
-export default class TabsContent{
-    name = null;
-    content = null;
-
-    render(){
-
-    }
-}
