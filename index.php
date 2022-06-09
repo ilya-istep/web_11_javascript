ecd18f46cfbac5d620bf9107234160abc7695746
Mihail_Zakharov <zmisa084@gmail.com>
2022-06-09 21:18:08 +0300

    материалы бэка с sql

--- index.php
+++ index.php
@@ -1,72 +1,32 @@
-<?php 
-	session_start();
+<?php
 
-	include_once 'class/Order.php';
-	include_once 'class/Product.php';
+    include_once 'DB.php';
 
-	$objProducts = new Product();
+    $db = new DB();
 
-	$products = $objProducts->render();
+    $nameObj = $db->select('users');
+
+    $nameObj = $db->select('users');
+
+    $name = $nameObj[0]['name'];
+
+    // echo "<pre>";
+    // print_r($db->select('users'));
+    // echo "</pre>";
+
+    echo "<pre>";
+    print_r($db->select('orders'));
+    echo "</pre>";
+
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-	<meta charset="UTF-8">
-	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<title>Document</title>
-	<style>
-		.wrap{
-			display: flex;
-			flex-wrap: wrap;
-		}
-		.wrap > div{
-			width: 20%;
-		}
-		.wrap > div img{
-			display: block;
-			width: 80%;
-			margin:  0 auto;
-		}
-		.order{
-			position: absolute;
-			right:10px;
-			top: 0;
-		}
-	</style>
-</head>
-<body>
 
-	<div class="order">
-
-		<p> 
-			<a href="obr.php?show_order=1">
-				В корзине
-				<?php 
-
-					echo Order::writeCountProductOrder();
+    <table>
+        <tr>
+            <td>
+                <p><?php echo $name; ?></p>
+            </td>
+        </tr>
+    </table>
 
 
-					// echo "<pre>";
-					// print_r($_SESSION['orders']);
-					// echo "</pre>";
-				?>
-				 товаров
-			<a>
-		</p>
-		<a href="obr.php?delete=1">Очистить корзину</a>
-	</div>
 
-	<div class="wrap">
-		<?php foreach ($products as $product):?>
-
-			<div>
-				<h5><?php echo $product['name']?></h5>
-				<img src="images/<?php echo $product['image']?>" alt="">
-				<a href="obr.php?product_id=<?php echo $product['id'];?>">В корзину</a>
-			</div>
-
-		<?php endforeach;?>
-	</div>
-
-</body>
-</html>
